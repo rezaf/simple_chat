a11acb6549d7eb1b5a26f37c5ae6a37fac446846
rezaf <reza.farajian@gmail.com>
2017-08-13 13:40:48 -0400

    Add model-level validations and associations

--- models/message.rb
+++ models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User'
+  validates :sender, :recipient, :body, presence: true
 end
--- models/user.rb
+++ models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :messages
+  validates :name, presence: true, uniqueness: true
 end
