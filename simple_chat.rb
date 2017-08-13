b3e7529b2678a0e0ca490bcaa90665aa59c58da0
rezaf <reza.farajian@gmail.com>
2017-08-13 18:53:22 -0400

    Update get user messages

--- simple_chat.rb
+++ simple_chat.rb
@@ -14,9 +14,14 @@ set :database, {adapter: "sqlite3", database: "simple_chat.sqlite3"}
 # Read messages for user from DB
 # Send json of messages to client
 # {id: {name:, received_bool:, body:, time:}}
-get '/messages/:id' do
+get '/user/:id/messages' do
   user_id = params[:id]
 
+  if user = User.find_by_id(user_id)
+    # Respond with user messages
+  else
+    {error: 'Cannot find user!'}.to_json
+  end
 end
 
 # {
