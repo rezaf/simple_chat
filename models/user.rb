2ae7384b26143f835825235a6fe6ca3b29e8d67f
rezaf <reza.farajian@gmail.com>
2017-08-13 13:59:19 -0400

    Fix associations on User model

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-  has_many :messages
+  has_many :sent_messages,
+      class_name: 'Message',
+      foreign_key: :sender_id
+
+  has_many :received_messages,
+      class_name: 'Message',
+      foreign_key: :recipient_id
+
   validates :name, presence: true, uniqueness: true
 end
