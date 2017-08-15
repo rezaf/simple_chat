8ee42f097bfa01d90f3bf28f3fcabb1b09f71b70
rezaf <reza.farajian@gmail.com>
2017-08-14 20:42:53 -0400

    Add User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,45 @@
 RSpec.describe 'User' do
   describe 'validations' do
     it 'validates name for presence' do
+      valid_user = User.new(name: 'John')
+      invalid_user = User.new(name: nil)
 
+      expect(valid_user).to be_valid
+      expect(invalid_user).to_not be_valid
     end
 
     it 'validates name for uniqueness' do
+      valid_user = User.new(name: 'Jack')
+      invalid_user = User.new(name: 'Jon')
 
+      expect(valid_user).to be_valid
+      expect(invalid_user).to_not be_valid
     end
   end
 
   describe '#messages' do
-    it 'returns a hash of sent and received messages' do
+    let(:user) { User.find_by(name: 'Jon') }
+    subject(:messages) { user.messages }
 
+    it 'returns a hash with sent and received keys' do
+      expect(messages).to be_a Hash
+      expect(messages).to include(:sent, :received)
+    end
+
+    it 'returns sent messages in the correct format' do
+      expect(messages[:sent].first).to include(
+        :timestamp,
+        recipient: 'Jill',
+        body: 'Hi!'
+      )
+    end
+
+    it 'returns received messages in the correct format' do
+      expect(messages[:received].first).to include(
+        :timestamp,
+        sender: 'Jill',
+        body: 'Good day!'
+      )
     end
   end
 end
