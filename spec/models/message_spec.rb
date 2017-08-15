a51f39a151ddd09444dcce40ebf78e5e83e550ea
rezaf <reza.farajian@gmail.com>
2017-08-14 20:48:54 -0400

    Add Message model specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,13 +1,19 @@
 RSpec.describe 'Message' do
-  it 'validates sender for presence' do
+  let(:jon) { User.find_by(name: 'Jon') }
+  let(:jill) { User.find_by(name: 'Jill') }
 
+  it 'validates sender for presence' do
+    message = Message.new(sender: nil, recipient: jill, body: 'Testing 1, 2')
+    expect(message).to be_invalid
   end
 
   it 'validates recipient for presence' do
-
+    message = Message.new(sender: jon, recipient: nil, body: 'Testing 1, 2')
+    expect(message).to be_invalid
   end
 
   it 'validates body for presence' do
-
+    message = Message.new(sender: jon, recipient: jill, body: nil)
+    expect(message).to be_invalid
   end
 end
